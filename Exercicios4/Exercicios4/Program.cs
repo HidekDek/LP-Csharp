2bcba58899b180bea6b73caa228151b8def3edd7
HidekDek <jvictorhidek@gmail.com>
2026-05-22 14:51:30 +0100

    Exercicio 4

---
+++ Exercicios4/Exercicios4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Exercicio4
+{
+    public static void Main(string[] args)
+    {
+        int idade = 8;
+
+        if (idade < 12)
+        {
+            Console.WriteLine("Você é uma criança.");
+        }
+        else if (idade >= 12 && idade < 18)
+        {
+            Console.WriteLine("Você é um adolescente.");
+        }
+        else
+        {
+            Console.WriteLine("Você é um adulto.");
+        }
+
+
+
+
+    }
+}
