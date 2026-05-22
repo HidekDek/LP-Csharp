d9cce8077ac258c4b0d3a959b0a993c3c7b9ef4f
HidekDek <jvictorhidek@gmail.com>
2026-05-22 15:43:59 +0100

    Exercicio 5

---
+++ Exercicio5/Exercicio5/Exercicio5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Exercicio4
+{
+    public static void Main(string[]args)
+    {
+        int idade = 45;
+        bool ecasado = true;
+
+        if (idade >= 65)
+        {
+            Console.WriteLine("Você é idoso.");
+        }
+        else if (idade < 65 && idade >= 18)
+        {
+            if (ecasado = false)
+            {
+                Console.WriteLine("Você é adulto e solteiro.");
+            }
+            else
+            {
+                Console.WriteLine("Você é adulto e casado.");
+            }
+        }
+
+        else if (idade < 18)
+        {
+            Console.WriteLine("Você é menor de idade.");
+        }
+    }
+}
