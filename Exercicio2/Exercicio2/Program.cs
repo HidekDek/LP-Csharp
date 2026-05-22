28a7a5e1afe3c4d74e2dd31cd06a42677764a3ac
HidekDek <jvictorhidek@gmail.com>
2026-05-22 11:54:12 +0100

    Exercicio 2

---
+++ Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Exercicio2
+{
+    public static void Main(string[] args)
+    {
+        double raio = 9;
+        const double PI = 3.14;
+
+        double area = raio * raio * PI;
+
+        Console.WriteLine("O raio do círculo é: " + raio);
+        Console.WriteLine("Logo, a área do círculo é: " + area+"m2");
+    }
+}
