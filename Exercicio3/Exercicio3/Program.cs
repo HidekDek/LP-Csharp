19e165b45cd5adea76fd2f42d8de7d8b0af931ab
HidekDek <jvictorhidek@gmail.com>
2026-05-22 12:33:24 +0100

    Exercicio 3

---
+++ Exercicio3/Exercicio3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Exercicio3
+{
+    public static void Main(string[] args)
+    {
+        int num1 = 42;
+        int num2 = 27;
+
+        if (num1 > num2)
+        {
+            Console.WriteLine("O número maior é: " + num1);
+        }
+        else if (num2 > num1)
+        {
+            Console.WriteLine("O número maior é: " + num2);
+        }
+        else
+        {
+            Console.WriteLine("Os números são iguais.");
+        }
+    }
+}
