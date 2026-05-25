68d5c905d6333c903c086dbb63a0ba97dddaa01f
HidekDek <jvictorhidek@gmail.com>
2026-05-25 11:36:26 +0100

    Exercicio6

---
+++ Exercicio6/Exercicio6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public  class Exercicio6
+{
+    public static void Main(string[] args)
+    {
+        int diaSemana = 1;
+        
+        switch (diaSemana)
+        {
+            case 1:
+                Console.WriteLine("Segunda-feira");
+                break;
+            case 2:
+                Console.WriteLine("Terça-feira");
+                break;
+            case 3:
+                Console.WriteLine("Quarta-feira");
+                break;
+            case 4:
+                Console.WriteLine("Quinta-feira");
+                break;
+            case 5:
+                Console.WriteLine("Sexta-feira");
+                break;
+            case 6:
+                Console.WriteLine("Sábado");
+                break;
+            case 7:
+                Console.WriteLine("Domingo");
+                break;
+            default:
+                Console.WriteLine("Número inválido. Tem que ser de 1 á 7.");
+                break;
+        }
+    }
+}
