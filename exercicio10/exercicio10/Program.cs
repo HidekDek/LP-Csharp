348c98adaa03ef061b472a0b972584830b387988
HidekDek <jvictorhidek@gmail.com>
2026-06-15 12:22:32 +0100

    exercicio10

---
+++ exercicio10/exercicio10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+public class exercicio10
+{
+    public static void Main(string[] args)
+    {
+        int soma = 0;
+
+        for (int contador = 0; contador <= 50; contador++)
+        {
+            
+            if (contador % 2 != 0)
+            {
+                soma += contador;
+             
+            }
+          
+        }
+        Console.WriteLine("Soma: " + soma);
+    }
+}
