936dc7d837cff44485dd9c11796d6f32743585db
HidekDek <jvictorhidek@gmail.com>
2026-06-19 09:38:33 +0100

    exercicio11

---
+++ novoExercicio11/novoExercicio11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+public class Exercicio11
+{
+    public static void Main(string[] args)
+    {
+        int i = 3;
+        int multi = 1;
+        while (i <= 30)
+        {
+            Console.WriteLine("3 x " + multi + " = " + i);
+            i += 3;
+            multi++;
+        }
+    }
+}
