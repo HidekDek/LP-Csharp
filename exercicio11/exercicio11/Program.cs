d6696ae98a72d4e12104c94fbd87232ef0486940
HidekDek <jvictorhidek@gmail.com>
2026-06-19 09:18:01 +0100

    exercicio11

---
+++ exercicio11/exercicio11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+public class exercicio11
+{
+    public static void Main(string[] args)
+    {
+        int i = 3;
+
+        while (i <= 30)
+        {
+            Console.WriteLine(i);
+            i += 3;
+        }
+    }
+}
