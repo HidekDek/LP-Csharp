05616984c26d0088518cd314acf586664f565d74
HidekDek <jvictorhidek@gmail.com>
2026-06-22 10:26:32 +0100

    exercicio12

---
+++ Exercicio12/Exercicio12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Exercicio11
+{
+    public static void Main(string[] args)
+    {
+        int i = 0;
+        int somaFinal = 0;
+
+        for (i = 0; somaFinal <= 100; i++)
+        {
+            somaFinal += i;
+            Console.WriteLine(somaFinal);
+        }
+        Console.WriteLine("O ultimo valor adicionado é: " + i);
+    }
+}    
